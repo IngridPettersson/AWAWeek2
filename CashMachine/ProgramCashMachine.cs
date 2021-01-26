48ba4e991d86bfbf6a798d8acc2d007a0ac7d5f9
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-26 12:32:23 +0100

    Add methods

--- CashMachine/ProgramCashMachine.cs
+++ CashMachine/ProgramCashMachine.cs
@@ -5,41 +5,50 @@ namespace CashMachine
 {
     class ProgramCashMachine
     {
+        static string[] transactions = new string[3];
         static void Main(string[] args)
         {
+            double balance = 1000;
+            bool exitProgram = false;
 
-            Console.WriteLine("I am your ATM. Press key D, W, B or T to choose what you want to do:\n");
-            Console.WriteLine("[D]eopist");
-            Console.WriteLine("[W]ithdrawal");
-            Console.WriteLine("[B]alance");
-            Console.WriteLine("[T]erminate");
-            Console.WriteLine();
-            ConsoleKey input = Console.ReadKey(true).Key;
 
-            switch (input)
+
+            do
             {
-                case ConsoleKey.D:
-                    MakeDeposit();
-                    break;
-                case ConsoleKey.W:
-                    MakeWithdrawal();
-                    break;
-                case ConsoleKey.B:
-                    Console.Write("Your current balance is: \n");
-                    ShowSaldo();
-                    break;
-                case ConsoleKey.T:
-                    Console.Write("Are you sure you want to terminate? \n");
-                    TerminateProgram();
-                    break;
-                default:
-                    break;
-            }
+                Console.WriteLine("I am your ATM. Press key D, W, B or T to choose what you want to do:\n");
+                Console.WriteLine("[D]eopist");
+                Console.WriteLine("[W]ithdrawal");
+                Console.WriteLine("[B]alance");
+                Console.WriteLine("[S]how Transactions");
+                Console.WriteLine("[T]erminate");
+                Console.WriteLine();
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                switch (input)
+                {
+                    case ConsoleKey.D:
+                        balance = MakeDeposit(balance);
+                        break;
+                    case ConsoleKey.W:
+                        balance = MakeWithdrawal(balance);
+                        break;
+                    case ConsoleKey.B:
+                        ShowSaldo(balance);
+                        break;
+                    case ConsoleKey.S:
+                        ShowTransactions();
+                        break;
+                    case ConsoleKey.T:
+                        exitProgram = TerminateProgram();
+                        break;
+                    default:
+                        break;
+                }
+            } while (!exitProgram);
         }
 
-        private static double MakeDeposit()
+        private static double MakeDeposit(double balance)
         {
-            double balance = 1000;
             int value;
 
             Console.Write("Enter amount you want to deposit: ");
@@ -58,16 +67,16 @@ namespace CashMachine
                 balance += value;
                 Console.WriteLine($"Current balance: {balance:C}");
             }
-
+            Console.WriteLine();
+            Console.WriteLine();
             return balance;
         }
 
-        private static double MakeWithdrawal()
+        private static double MakeWithdrawal(double balance)
         {
-            double balance = 1000;
             int value;
 
-            Console.Write("Enter amount you want to withdraw: \n");
+            Console.Write("Enter amount you want to withdraw: ");
             bool isInt = int.TryParse(Console.ReadLine(), out value);
             bool enoughMoney = balance >= value;
 
@@ -93,17 +102,39 @@ namespace CashMachine
                 balance -= value;
                 Console.WriteLine($"Current balance: {balance:C}");
             }
+            Console.WriteLine();
+            Console.WriteLine();
             return balance;
         }
 
-        private static void ShowSaldo()
+        private static void ShowSaldo(double balance)
         {
-            Console.WriteLine("Show saldo...");
+            Console.Write($"Your current balance is: {balance:C}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        private static void ShowTransactions()
+        {
+            throw new NotImplementedException();
         }
 
-        private static void TerminateProgram()
+        private static bool TerminateProgram()
         {
-            Console.WriteLine("Terminate program...");
+            bool exitProgram;
+            Console.WriteLine("Are you sure you want to terminate? ");
+            Console.WriteLine();
+            Console.WriteLine("[Y]es");
+            Console.WriteLine("[N]o");
+            if (ConsoleKey.Y == Console.ReadKey(true).Key)
+            {
+                exitProgram = true;
+            }
+            else
+            {
+                exitProgram = false;
+            }
+
+            return exitProgram;
         }
     }
 }
