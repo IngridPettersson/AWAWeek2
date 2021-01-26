e60d5be186be71e1a62310e7712966df944f6b97
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-26 20:25:47 +0100

    Add Epidemic Project

---
+++ Epidemic/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epidemic
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public bool Infected { get; set; }
+        public int InfectedWhen { get; set; }
+        public bool Immune { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Epidemic/ProgramEpidemic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epidemic
+{
+    class ProgramEpidemic
+    {
+        static List<Person> discoDancers = new List<Person>();
+        static void Main(string[] args)
+        {
+            AddPeopleToList();
+        }
+
+        private static void AddPeopleToList()
+        {
+            for (int i = 0; i < 51; i++)
+            {
+                string dancer = GetDancerName(i);
+                
+                discoDancers.Add(new Person(dancer));
+            }
+        }
+
+        private static string GetDancerName(int i)
+        {
+            string dancerName = $"dancer{i}";
+            return dancerName;
+        }
+    }
+}
