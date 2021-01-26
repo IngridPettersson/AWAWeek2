680324cb65521f11408ada0c7bc8941fcd6f9943
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-26 11:59:48 +0100

    Add methods

--- CashMachine/ProgramCashMachine.cs
+++ CashMachine/ProgramCashMachine.cs
@@ -7,52 +7,103 @@ namespace CashMachine
     {
         static void Main(string[] args)
         {
-            // Ge användaren alternativen.
-            // Sätta de olika alternativen som variabler.
-            // Göra ett switch statement för varje alternativ och vad som då ska hända.
-            // 
 
-            //            …med hjälp av en switch sats skapar en inmatningsmeny för en bankomat. Alternativen ska
-            //            vara:
-            //            [I]nsättning
-            //            [U]ttag
-            //            [S]aldo
-            //            [A]vsluta
-
-            Console.WriteLine("I am your ATM. Enter key D, W, B or E\n");
+            Console.WriteLine("I am your ATM. Press key D, W, B or T to choose what you want to do:\n");
             Console.WriteLine("[D]eopist");
             Console.WriteLine("[W]ithdrawal");
             Console.WriteLine("[B]alance");
             Console.WriteLine("[T]erminate");
             Console.WriteLine();
-            ConsoleKey input = Console.ReadKey().Key;
+            ConsoleKey input = Console.ReadKey(true).Key;
 
-            //List<ConsoleKeyInfo> validKeys = new List<ConsoleKeyInfo>();
-            //validKeys.Add(ConsoleKeyInfo.I);
-            //validKeys.Add(ConsoleKey.U);
-            //validKeys.Add(ConsoleKey.S);
-            //validKeys.Add(ConsoleKey.A);
-
-            //foreach (var key in validKeys)
-            //{
             switch (input)
             {
                 case ConsoleKey.D:
-                    Console.Write("Enter amount you want to deposit: ");
+                    MakeDeposit();
                     break;
                 case ConsoleKey.W:
-                    Console.Write("Enter amount you want to withdraw: ");
+                    MakeWithdrawal();
                     break;
                 case ConsoleKey.B:
-                    Console.Write("Your current balance is: ");
+                    Console.Write("Your current balance is: \n");
+                    ShowSaldo();
                     break;
-                case ConsoleKey.E:
-                    Console.Write("Are you sure you want to terminate? ");
+                case ConsoleKey.T:
+                    Console.Write("Are you sure you want to terminate? \n");
+                    TerminateProgram();
                     break;
                 default:
                     break;
             }
-            //}
+        }
+
+        private static double MakeDeposit()
+        {
+            double balance = 1000;
+            int value;
+
+            Console.Write("Enter amount you want to deposit: ");
+            bool isInt = int.TryParse(Console.ReadLine(), out value);
+
+
+            while (!isInt || value <= 0)
+            {
+                Console.Write("Enter a valid integer higher than 0: ");
+                isInt = int.TryParse(Console.ReadLine(), out value);
+            }
+
+            if (isInt && value > 0)
+            {
+                Console.WriteLine($"You have deposited {value:C}.");
+                balance += value;
+                Console.WriteLine($"Current balance: {balance:C}");
+            }
+
+            return balance;
+        }
+
+        private static double MakeWithdrawal()
+        {
+            double balance = 1000;
+            int value;
+
+            Console.Write("Enter amount you want to withdraw: \n");
+            bool isInt = int.TryParse(Console.ReadLine(), out value);
+            bool enoughMoney = balance >= value;
+
+            while (!isInt || value <= 0 || !enoughMoney)
+            {
+                if (!isInt || value <= 0)
+                {
+                    Console.Write("Enter a valid integer higher than 0: ");
+                    isInt = int.TryParse(Console.ReadLine(), out value);
+                    enoughMoney = balance >= value;
+                }
+                else if (!enoughMoney)
+                {
+                    Console.Write($"You do not have enough money in your account. Enter an amount of maximum {balance:C}: ");
+                    isInt = int.TryParse(Console.ReadLine(), out value);
+                    enoughMoney = balance >= value;
+                }
+            }
+
+            if (isInt && value > 0 && enoughMoney)
+            {
+                Console.WriteLine($"You have successfully withdrawn {value:C}.");
+                balance -= value;
+                Console.WriteLine($"Current balance: {balance:C}");
+            }
+            return balance;
+        }
+
+        private static void ShowSaldo()
+        {
+            Console.WriteLine("Show saldo...");
+        }
+
+        private static void TerminateProgram()
+        {
+            Console.WriteLine("Terminate program...");
         }
     }
 }
