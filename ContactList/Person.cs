430e7e1013c6ab1669588d183b64792d9838494e
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-28 13:49:24 +0100

    Add List Project

---
+++ ContactList/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList
+{
+    class Person
+    {
+
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+        public void AddPerson()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ ContactList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactList
+{
+    class Program
+    {
+        List<Person> contactList = new List<Person>(); // Vill man lägga listan i Main eller i klassen Person?
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We're gonna add contacts to your contact list! Press any key to add new contact.");
+            Console.ReadKey(true);
+            Person addedContact = AddContactInfo();
+            AddContactToList(addedContact);
+        }
+
+        static Person AddContactInfo()
+        {
+            Person contact = new Person();
+
+            Console.Write("Enter contacts first name: ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.Write("Add contacts phone number: ");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Add contacts email address: ");
+            contact.Email = Console.ReadLine();
+
+            return contact;
+        }
+
+        private static void AddContactToList(Person contact)
+        {
+            conta
+        }
+    }
+}
