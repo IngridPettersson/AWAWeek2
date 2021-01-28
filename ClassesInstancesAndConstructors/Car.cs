8b18b992f38836f0e608d41a0b6f3b61b9d833fa
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-28 09:51:41 +0100

    Add changes

--- ClassesInstancesAndConstructors/Car.cs
+++ ClassesInstancesAndConstructors/Car.cs
@@ -11,6 +11,20 @@ namespace ClassesInstancesAndConstructors
         public string Make { get; set; }
         public string TypeOfFuel { get; set; }
         public int YearOfManufacture { get; set; }
-        public int LicenseNumber { get; set; }
+        public string LicenseNumber { get; set; }
+
+        int currentSpeed;
+
+        public void IncreaseCurrentSpeed(int increase)
+        {
+            currentSpeed += increase;
+            VeryComplexInternalMethodToFeedFuelToEngine();
+        }
+
+        // ska vara privat
+        void VeryComplexInternalMethodToFeedFuelToEngine()
+        {
+            // komplexa saker
+        }
     }
 }
--- ClassesInstancesAndConstructors/ProgramConstructors.cs
+++ ClassesInstancesAndConstructors/ProgramConstructors.cs
@@ -6,7 +6,13 @@ namespace ClassesInstancesAndConstructors
     {
         static void Main(string[] args)
         {
-
+            Car myCar = new Car();
+            myCar.Brand = "Volvo";
+            myCar.LicenseNumber = "CWZ635";
+            myCar.TypeOfFuel = "Electric";
+            myCar.Make = "XC60";
+            myCar.YearOfManufacture = 2006;
+            myCar.IncreaseCurrentSpeed(2);
         }
     }
 }
