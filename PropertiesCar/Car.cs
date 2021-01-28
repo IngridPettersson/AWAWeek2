a044e5e9c8b51b0d8c768391325c778715b49944
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-28 20:12:52 +0100

    Add PropertiesCar Project

---
+++ PropertiesCar/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertiesCar
+{
+    class Car
+    {
+        private string licenseNumber;
+
+        public string LicenseNumber
+        {
+            get
+            {
+                string letters = licenseNumber.Substring(0, 3).ToUpper();
+                string numbers = licenseNumber.Substring(3);
+                return $"*** {letters} {numbers} ***";
+            }
+            set
+            {
+                int number;
+                bool isNumber = int.TryParse(value.Substring(3), out number);
+                if (value.Length == 6 && isNumber && number < 999 && number >= 0)
+                    licenseNumber = value;
+                else
+                    throw new Exception();
+            }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+
+    }
+}
---
+++ PropertiesCar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PropertiesCar
+{
+    class Program
+    {
+        static string inputLicenseNumber = "";
+        static bool looping = true;
+
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[100];
+            while (looping)
+            {
+                int i = 0;
+                AddCar(cars);
+                i++;
+            }
+
+            PrintOut(cars);
+        }
+
+        private static void PrintOut(Car[] cars)
+        {
+            for
+        }
+
+        private static void AddCar(Car[] cars)
+        {
+            Car car = new Car();
+            GetUserInput();
+            car.LicenseNumber = inputLicenseNumber;
+
+
+        }
+
+        public static void GetUserInput()
+        {
+            Console.WriteLine("Make your choice: \n");
+            Console.WriteLine("[A]dd car");
+            Console.WriteLine("[E]xit");
+            ConsoleKey keyPress = Console.ReadKey(true).Key;
+
+            if (keyPress == ConsoleKey.A)
+            {
+                Console.Write("\nEnter license number of car: ");
+                inputLicenseNumber = Console.ReadLine();
+                Console.WriteLine();
+            }
+            else
+                looping = false;
+
+        }
+    }
+}
